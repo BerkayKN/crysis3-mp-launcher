937f5edc026cfef41071c378463f265d7b29dc29
ErBuSlayer <93136716+ErBuSlayer@users.noreply.github.com>
2025-09-26 21:29:24 +0300

    Update MainWindow.cs

--- MainWindow.cs
+++ MainWindow.cs
@@ -37,7 +37,7 @@ namespace C2COMMUNITY_Mod_Launcher
         private const string GAME_MOD_FOLDER = "C3MP";
         private const string SERVER_MOD_PATH = "/C3MP/ModFiles/";
         private const string SERVER_LIST_SOURCE_URL = "https://openspy-website.nyc3.digitaloceanspaces.com/servers/capricorn.json";
-        private const string GAME_STARTER_FILE_NAME = "Crysis3.exe";
+        private const string GAME_STARTER_FILE_NAME = "Crysis 3 - Mod C3MP.bat";
         private const string GET_SERVER_URL = "https://raw.githubusercontent.com/BerkayKN/crysis2-mp-launcher/main/server/server.txt";
         //End of Defines
 
